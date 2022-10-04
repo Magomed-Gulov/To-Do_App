ca229833210ab28852a4508783dbb4d990690351
Magomed-Gulov <106895801+Magomed-Gulov@users.noreply.github.com>
2022-10-04 14:43:58 +0300

    To Do List App 0.1

--- app/todoapp/models.py
+++ app/todoapp/models.py
@@ -1,5 +1,16 @@
+from email.policy import default
+from django.utils import timezone
 from django.db import models
 
+import datetime
 
-class TodoListItem(models.Model):
-    content = models.TextField()
+
+class TodoListItem( models.Model ):
+    content = models.TextField(  )
+    pub_data = models.DateTimeField( auto_now=False, default=timezone.now )
+    # print( timezone.now( ) )
+    status = models.BooleanField( default=False )
+
+    # def was_published_recently( self ):
+    #     now = timezone.now(  )
+    #     return now - datetime.timedelta( days=1 ) <= self.pub_date <= now
--- app/todoapp/views.py
+++ app/todoapp/views.py
@@ -6,16 +6,22 @@ from .models import TodoListItem
 
 def todoappView(request):
     all_todo_items = TodoListItem.objects.all()
-    return render(request, 'todolist.html',
+    return render(request, 'todoapp/todolist.html',
     {'all_items':all_todo_items})
 
 def addTodoView(request):
-    x = request.POST['content']
-    new_item = TodoListItem(content = x)
-    new_item.save()
+    x = str(request.POST['content']).strip()
+    if x != "":
+        new_item = TodoListItem(content = x)
+        new_item.save()
     return HttpResponseRedirect('/todoapp/')
 
-def deleteTodoView(request, i):
+def actionTodoView(request, te, i):
     y = TodoListItem.objects.get(id= i)
-    y.delete()
+
+    if te == 'status':
+        y.status = not y.status
+        y.save_base()
+    elif te == 'delete':
+        y.delete()
     return HttpResponseRedirect('/todoapp/')
